61db15b8d135c6cfea24995f2530cda2f17d3f2e
nkane312 <nkane312@gmail.com>
2016-11-22 16:31:44 -0600

    Updates salutation, return path pixels, updates remove-html.pipe.ts

--- src/app/remove-html.pipe.ts
+++ src/app/remove-html.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RemoveHtmlPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
+    if (args === 'space') {
+      value = value.replace(/<([br /]{1,})>/g, ' ');
+    }
     if (value) {
       value = value.replace(/<([a-zA-Z\d\s\/\=\"\"\:\.]{1,})>/g, '');
     }
