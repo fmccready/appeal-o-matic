c1946e23f70e056522956c46bdf004f27c9265df
nkane312 <nkane312@gmail.com>
2017-03-29 16:18:21 -0500

    Updates the removeHtml pipe to include &lsquo; for the current templates

--- src/app/remove-html.pipe.ts
+++ src/app/remove-html.pipe.ts
@@ -5,12 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class RemoveHtmlPipe implements PipeTransform {
 
-  transform(value: any, args?: any, args2?: any): any {
+  transform(value: any, args?: any, args2?: any, args3?: any): any {
     if (args === 'space') {
       value = value.replace(/<([br /]{1,})>/g, ' ');
     }
     if (args2 === 'rsquo') {
       value = value.replace(/&rsquo;/g, '’')
+    }
+    if (args3 === 'lsquo') {
+      value = value.replace(/&lsquo;/g, '‘')
     }
     if (value) {
       value = value.replace(/<([a-zA-Z\d\s\/\=\"\"\:\.]{1,})>/g, '');
